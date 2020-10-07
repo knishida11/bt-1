5b9d53d6d57ced39b348d31cdc8bbe699dbfb038
Keigo Nishida <keigo.nishida.1480@gmail.com>
2020-10-08 06:31:36 +0900

    deleted wow.js start instruction

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,6 @@ jQuery(function($) {
   // object-fit-images
   objectFitImages();
 
-  // wow.js
-  new WOW().init();
-
   // smooth scrolling
   $('a[href^="#"]').click(function () {
     var speed = 400;
